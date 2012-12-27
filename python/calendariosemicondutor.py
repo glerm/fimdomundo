9775576aa2bb9294192c6486f85c8b95b61a00ad
Glerm Soares <organismo@gmail.com>
2012-12-27 00:46:06 -0200

    extraindo as imagens do calendario

--- python/calendariosemicondutor.py
+++ python/calendariosemicondutor.py
@@ -5,5 +5,19 @@ l=("http://www.sincronariodapaz.org/altera/CalculoKin/crono1B.php?dianterior=24&
  
 page = urllib2.urlopen(l)
 soup = BeautifulSoup(page)
+t = soup.find("table")
 
-print soup
+#dat = [ map(str, row.findAll("td")) for row in t.findAll("tr") ]
+
+
+#print dat[0][0]
+
+imgs=[]
+
+for ana in soup.findAll('img'):
+	imgs.append("http://sincronariodapaz.org/altera/CalculoKin" +(ana["src"][1:]))
+
+
+print imgs
+
+#print soup.prettify()
